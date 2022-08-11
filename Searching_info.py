5dc9d7ba6d6a2b6e4139931fafb6db897f77c6d2
JorgeMioska <quispechilojorge@gmail.com>
2022-08-10 20:49:21 -0500

    New File

---
+++ Searching_info.py
@@ -0,0 +1,11 @@
+#Trying to fin the address using ONLY string methods
+
+Mary="From Rome-Italy, e-mail address maryling46@outlook.com"
+
+Mary_email=Mary.find("@")
+Mary_email_start=Mary.find("address ")
+
+"""print(Mary_email) #42
+print(Mary_email_start) #24
+print(len("address "))#8"""
+print(Mary[Mary_email_start+8:len(Mary)])
